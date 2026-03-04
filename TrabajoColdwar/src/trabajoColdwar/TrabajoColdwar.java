ea608a0fa17971c06076f7e3b14f53c4bb683c3b
anxonegr <76596858+anxonegr@users.noreply.github.com>
2026-03-04 14:44:50 +0100

    Update TrabajoColdwar.java

--- TrabajoColdwar/src/trabajoColdwar/TrabajoColdwar.java
+++ TrabajoColdwar/src/trabajoColdwar/TrabajoColdwar.java
@@ -1,7 +1,39 @@
 package trabajoColdwar;
+import java.util.Scanner;
 
 public class TrabajoColdwar {
-  /fasdgfsdg
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int opcion = 0;
+		
+		do {
+			
+			opcion = menu(sc);
+		
+		}while (opcion != 0);
+		sc.close();
+	}
+	
+	//Imprime el menú. Recibe el Scanner. Devuelve la opción seleccionada
+	public static int menu(Scanner sc) {
+
+		// Mostrar el menú
+        System.out.println("\n--- MENÚ PRINCIPAL ---");
+        System.out.println("1º JUGAR");
+        System.out.println("2º REGLAS DEL JUEGO");
+        System.out.println("3º INFORMACIÓN");
+        System.out.println("4º APARTADO ABIERTO");
+        System.out.println("0º SALIR");
+        
+        //Devuelve la opción seleccionada
+        System.out.print("\nSeleccione una opción: ");
+        return sc.nextInt();
+        
+		
+	}
 }
+
 
 
