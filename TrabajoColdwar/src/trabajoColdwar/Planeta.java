e2ee4e855e895274f8547f6f09a2f9085c47d977
netherwhom <louroc25@fpcoruna.afundacion.org>
2026-04-04 19:46:55 +0200

    Update Planeta.java

--- TrabajoColdwar/src/trabajoColdwar/Planeta.java
+++ TrabajoColdwar/src/trabajoColdwar/Planeta.java
@@ -2,6 +2,60 @@ package trabajoColdwar;
 
 public class Planeta {
 	
-	
-	
+	private int vidas;
+    private String nombre;
+    private int misilesRonda;
+    private static int numeroEquipos = 0;
+
+
+    //constructor
+
+    public Planeta(String nombre) {
+		
+        this.nombre = nombre;
+        this.vidas = 200;
+        this.misilesRonda = 50;
+        numeroEquipos++;
+
+    }
+
+
+    //getters y setters
+
+    public int getVidas() {
+        return vidas;
+
+    }
+
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+
+    }
+
+
+    public String getNombre() {
+        return nombre;
+
+    }
+
+
+    public int getMisilesRonda() {
+        return misilesRonda;
+
+    }
+
+
+    public void setMisilesRonda(int misilesRonda) {
+        this.misilesRonda = misilesRonda;
+
+    }
+
+
+    public static int getNumeroEquipos() {
+        return numeroEquipos;
+
+    }
+
 }
+
